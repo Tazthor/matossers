0dd7373c5fdd7f2be90f924f2a98c3ec4beb2827
Tazthor <sprat@omnium.cat>
2022-04-25 17:57:58 +0200

    Sort actuacions

--- components/GridCalendari.js
+++ components/GridCalendari.js
@@ -1,24 +1,56 @@
 import { Grid, Box, Text } from '@chakra-ui/react'
 import actuacions from '../public/data/actuacions.json'
 
-export const GridCalendari = function (props) {
-  var dateNow = new Date()
-  var dateUTCNow = new Date(dateNow.toUTCString())
+const DateFormat = function (data) {
+  const [day, month, year] = data.split('/');
+  var dateprov = year + '-' + month + '-' + day
+  var dataFormat = new Date(dateprov).toISOString()
+  return (dataFormat)
+}
 
+export const GridCalendari = function (props) {
+  var dateNow = new Date().toISOString()
   return (
     <Box w={["90%", "80%", "75%"]} m="auto">
-    Properes
+      Properes
       <Grid templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']} gap={4}>
         {
           actuacions.map((act, index) => {
-            return (
-              <Box key={index}>
-                <Text>{act.actuacio}</Text>
-              </Box>
-            )
+            var data = DateFormat(act.data_curta)
+            console.log('Act:' + data, 'now:' + dateNow)
+            if (data >= dateNow) {
+              return (
+                <Box key={index}>
+                  <Text>{act.actuacio}</Text>
+                </Box>
+              )
+            }
+            else {
+              return <></>
+            }
           })
         }
       </Grid>
+      Passades
+      <Grid templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)']} gap={4}>
+        {
+          actuacions.map((act, index) => {
+            var data = DateFormat(act.data_curta)
+            console.log('Act:' + data, 'now:' + dateNow)
+            if (data < dateNow) {
+              return (
+                <Box key={index}>
+                  <Text>{act.actuacio}</Text>
+                </Box>
+              )
+            }
+            else {
+              return <></>
+            }
+          })
+        }
+      </Grid>
+
     </Box>
   )
 }
