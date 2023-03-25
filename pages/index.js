82bc3d9d63db875b22d5209fbda7ae74a5d624af
Tazthor <sprat@omnium.cat>
2023-03-25 10:20:38 +0100

    blocs home

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import { Container } from '../components/Container'
-import Margin from '../components/Margin';
+import { Container } from "../components/Container";
+import Margin from "../components/Margin";
 import Navbar from "../components/Navbar";
 import HeaderHome from "../components/HeaderHome";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
+import Topics from '../components/Topics';
+import BlocText from '../components/BlocText';
 
 export default function Home() {
-
   return (
     <Container>
       <Navbar />
@@ -13,10 +14,16 @@ export default function Home() {
       <HeaderHome
         img="/images/home/header.jpg"
         imgMbl="/images/home/headermbl.jpg"
-        titleVisible={ true }
+        titleVisible={true}
       />
       <Margin desktop="40px" tablet="50px" mobile="20px" />
-      <Footer/>
+      <Topics />
+      <Margin desktop="40px" tablet="50px" mobile="20px" />
+      <BlocText />
+      <Margin desktop="40px" tablet="50px" mobile="20px" />
+
+      <Margin desktop="40px" tablet="50px" mobile="20px" />
+      <Footer />
     </Container>
-  )
+  );
 }
