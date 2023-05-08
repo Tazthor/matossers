1e72c5ea8f7dce76e7b8c6e8d54a366137b4a776
Tazthor <sprat@omnium.cat>
2023-05-08 12:01:37 +0200

    arranjament propera act de grid actuacions

--- components/GridCalendari.js
+++ components/GridCalendari.js
@@ -4,14 +4,14 @@ import Margin from "./Margin";
 import Title from "./Title";
 import ActuacioCard from "./ActuacioCard";
 
-export const GridCalendari = function ({ actuacions }) {
+export const GridCalendari = function ({ actuacions, properaOnly }) {
   var dateNow = new Date();
   const [isSorted, setIsSorted] = useState(false);
 
   var properaAct = actuacions.find(
     (actuacio) => actuacio.data.toDate() >= dateNow
   );
-
+  console.log(properaAct);
   useEffect(() => {
     actuacions = actuacions.sort((a, b) =>
       a.data > b.data ? 1 : b.data > a.data ? -1 : 0
@@ -21,45 +21,52 @@ export const GridCalendari = function ({ actuacions }) {
 
   return (
     <Box w={{ base: "90%", md: "80%", xl: "75%" }} m="auto">
-      <Title header="2" text="Properes actuacions"></Title>
+      <Title header="2" text={(properaOnly) ? "Propera actuació" : "Properes actuacions"}></Title>
       <Margin desktop="40px" mobile="10px" />
       {(properaAct == undefined && (
         <Text>Actualment no hi ha actuacions previstes al calendari</Text>
-      )) || (
-        <Grid
-          templateColumns={{
-            base: "repeat(1, 1fr)",
-            md: "repeat(2, 1fr)",
-            xl: "repeat(3, 1fr)",
-          }}
-          gap={4}
-        >
-          {isSorted &&
-            actuacions.map((act, index) => {
-              if (act.data.toDate() >= dateNow) {
-                return <ActuacioCard key={index} act={act} type="futures" />;
-              }
-            })}
-        </Grid>
+      )) ||
+        (properaOnly && <ActuacioCard act={properaAct} type="futures" />) || (
+          <Grid
+            templateColumns={{
+              base: "repeat(1, 1fr)",
+              md: "repeat(2, 1fr)",
+              xl: "repeat(3, 1fr)",
+            }}
+            gap={4}
+          >
+            {isSorted &&
+              actuacions.map((act, index) => {
+                if (act.data.toDate() >= dateNow) {
+                  return <ActuacioCard key={index} act={act} type="futures" />;
+                }
+              })}
+          </Grid>
+        )}
+      {!properaOnly && (
+        <>
+          <Margin desktop="40px" mobile="30px" />
+          <Title header="2" text="Darreres actuacions"></Title>
+          <Margin desktop="40px" mobile="10px" />
+          <Grid
+            templateColumns={{
+              base: "repeat(1, 1fr)",
+              md: "repeat(2, 1fr)",
+              xl: "repeat(4, 1fr)",
+            }}
+            gap={4}
+          >
+            {isSorted &&
+              actuacions.map((act, index2) => {
+                if (act.data.toDate() < dateNow) {
+                  return (
+                    <ActuacioCard key={index2} act={act} type="passades" />
+                  );
+                }
+              })}
+          </Grid>
+        </>
       )}
-      <Margin desktop="40px" mobile="30px" />
-      <Title header="2" text="Darreres actuacions"></Title>
-      <Margin desktop="40px" mobile="10px" />
-      <Grid
-        templateColumns={{
-          base: "repeat(1, 1fr)",
-          md: "repeat(2, 1fr)",
-          xl: "repeat(4, 1fr)",
-        }}
-        gap={4}
-      >
-        {isSorted &&
-          actuacions.map((act, index2) => {
-            if (act.data.toDate() < dateNow) {
-              return <ActuacioCard key={index2} act={act} type="passades" />;
-            }
-          })}
-      </Grid>
     </Box>
   );
 };
