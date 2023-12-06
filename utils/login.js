21834e47a2abe212d6f610ad17fa69d4e3f90aaf
Tazthor <sprat@omnium.cat>
2023-12-06 11:46:12 +0100

    update env

--- utils/login.js
+++ utils/login.js
@@ -19,14 +19,14 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGtJC973MQxS5V7ZISBHfjmZICFAxz4g4",
-  authDomain: "matossers.firebaseapp.com",
-  databaseURL: "https://matossers.firebaseio.com",
-  projectId: "matossers",
-  storageBucket: "matossers.appspot.com",
-  messagingSenderId: "76874283575",
-  appId: "1:76874283575:web:4995df3102552b0dde1db2",
-  measurementId: "G-Y1L5YSPM4B",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_databaseURL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
--- utils/utils.js
+++ utils/utils.js
@@ -4,14 +4,14 @@ import { collection, doc, getDocs, setDoc, Timestamp} from "firebase/firestore";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGtJC973MQxS5V7ZISBHfjmZICFAxz4g4",
-  authDomain: "matossers.firebaseapp.com",
-  databaseURL: "https://matossers.firebaseio.com",
-  projectId: "matossers",
-  storageBucket: "matossers.appspot.com",
-  messagingSenderId: "76874283575",
-  appId: "1:76874283575:web:4995df3102552b0dde1db2",
-  measurementId: "G-Y1L5YSPM4B",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_databaseURL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId,
 };
 
 export function initApp() {
